0fe2591640baf4721d3a17c3118b3c2db0f1d9d1
ChandraBudiWijaya <chandrabw.cjcc@gmail.com>
2024-09-16 14:50:08 +0700

    build
    add folder and setup

---
+++ components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+'use client'
+import React from 'react'
+
+function Footer() {
+
+    return (
+        <div>
+
+        </div>   
+    )
+}
+
+export default Footer
---
+++ components/umkmCard/UmkmCard.jsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import React from 'react'
+
+function UmkmCard() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default UmkmCard
